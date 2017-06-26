72c318400f51ee6132bb24edce88684439555c4a
Haley F. Sanders <hfsanders@pbs.org>
2017-06-26 17:01:12 -0500

    added smart crop onto crop transformation, changed params of process_transforms, now pass filename from application_new_api.py to pipeline

--- its/pipeline.py
+++ its/pipeline.py
@@ -5,41 +5,39 @@ Script to apply transformations to validated images.
 from transformations import BaseTransform, ResizeTransform
 
 
-def process_transforms(img, *transforms):
+def process_transforms(img, transforms, *args):
 
     """
     View that returns an image transformed according to the
     query options in the request string.
     Return URI to transformed image.
     """
-    new_img = None
     transform_classes = BaseTransform.__subclasses__()
     first_applied = ['resize']
-    transforms = transforms[0]
+    img_info = args
+
     # check if a similar transform on the same image is already in cache
 
     if len(transforms) == 0:  # no transforms; return image as is
-        new_img = img
-        return new_img
+        return img
 
     # apply necessary/easily applicable transforms
     # according to preset precedence
     if "resize" in transforms.keys():
-        new_img = ResizeTransform.apply_transform(img, transforms['resize'])
+        img = ResizeTransform.apply_transform(img, transforms['resize'],img_info)
 
     for tclass in transform_classes:
         if tclass.slug in transforms.keys():
             if tclass.slug not in first_applied:
-                new_img = tclass.apply_transform(img, transforms[tclass.slug])
+                
+                img = tclass.apply_transform(img, transforms[tclass.slug], img_info)
 
-    # recursively apply transformations all other transformations
-    # result_img = ITSTransformer.apply_transformations(transforms_dict)
     # cache resulting image
     # return the image in browser
     return "completed"
 
 
-def apply_multiple_transforms(img, transformations_dict):
+# def apply_multiple_transforms(img, transformations_dict):
 
     """
     Recursively applies image transformations.
