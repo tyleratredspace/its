35745bdd43690b4f3f3fb3e8ec4cc61edd5ce19f
Haley F. Sanders <hfsanders@pbs.org>
2017-06-26 15:26:35 -0500

    revised crop to optionally take in x & y coords for focal

--- its/transformations/crop.py
+++ its/transformations/crop.py
@@ -9,21 +9,28 @@ class CropTransform(BaseTransform):
 
     def apply_transform(img, *args):
         """
-        Crops input img using 
+        Crops input img about a focal point.
+        The default focal point is the center of the image.
         """
-
-        center = [floor(img.size[0]/2), floor(img.size[1]/2)]
-
+        crop_args = args[0].split('x')
         try:
-            cwidth = int(args[0].split('x')[0])
-            cheight = int(args[0].split('x')[1])
+            crop_size = [int(crop_args[0]), int(crop_args[1])]
+
+            if len(crop_args) == 2:
+                focal = [floor(img.size[0]/2), floor(img.size[1]/2)]
+            else:
+                focal = [int(crop_args[2]), int(crop_args[3])]
         except Exception as e:
             abort(400)
 
         try:
-            img = img.crop([center[0] - cwidth, center[1] - cheight, center[0] + cwidth, center[1] + cheight])
-        except Exception as e:
-            raise e
 
-        img.show()
+            if len(crop_args) == 2:
+                img = img.crop([focal[0] - crop_size[0], focal[1] - crop_size[1], focal[0] + crop_size[0], focal[1] + crop_size[1]])
+            else:
+                img = img.crop([focal[0], focal[1], focal[0] + crop_size[0], focal[1] + crop_size[1]])
+            
+        except Exception as e:
+            abort(304)
+
         return img
