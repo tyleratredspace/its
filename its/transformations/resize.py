9d9a57449b36ea61dd2cf280a03c766d0ea28333
Haley F. Sanders <hfsanders@pbs.org>
2017-06-26 13:47:15 -0500

    Resize accepts single argument and raises HTTP error responses when appropriate

--- its/transformations/resize.py
+++ its/transformations/resize.py
@@ -1,5 +1,7 @@
 from .base import BaseTransform
 from PIL import Image
+from math import floor
+from flask import Flask, abort
 
 
 class ResizeTransform(BaseTransform):
@@ -7,12 +9,34 @@ class ResizeTransform(BaseTransform):
     slug = "resize"
 
     def apply_transform(img, *args):
-
-        size = [int(size_arg) for size_arg in args[0].split('x')]
+        """
+        Resizes input image while maintaining aspect ratio.
+        """
 
         try:
-            img.thumbnail(size, Image.ANTIALIAS)
+            rwidth= args[0].split('x')[0]
+            rheight = args[0].split('x')[1]
+            size_ratio = img.size[0] / img.size[1]
         except Exception as e:
-            raise e
+            # print("Resize takes WWxHH, WWx, or xHH, where WW is the requested width and HH is the requested height.")
+            abort(400)
+
+        try:
+            if rwidth != '' and rheight != '':
+                rwidth = int(rwidth)
+                rheight = int(rheight)
+            elif rwidth == '' and rheight != '':
+                rheight = int(rheight)
+                rwidth = floor(size_ratio * rheight)
+            elif rheight == '' and rwidth != '':
+                rwidth = int(rwidth)
+                rheight = floor(size_ratio * rwidth)
+        except Exception as e:
+            abort(400)
+        
+        try:
+            img.thumbnail([rwidth,rheight], Image.ANTIALIAS)
+        except Exception as e:
+            abort(304)
 
         return img
