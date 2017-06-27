3126405ab04e5f70b6ec3ebbe9d2ede665ddcfbe
Haley F. Sanders <hfsanders@pbs.org>
2017-06-27 15:04:02 -0500

    filled out stubs in application.py, removed application_old_api.py.

--- its/application.py
+++ its/application.py
@@ -16,53 +16,134 @@ def index():
 # image transform command
 @app.route('/<namespace>/<path:filename>', methods=['GET'])
 def transform_image(namespace, filename):
-    query = request.args
+    query = request.args.to_dict()
+    query['filename'] = filename
 
     image = load_image(namespace, filename)
 
-    if image:
+    if image is None:
         abort(404)
 
     # transformation processor call
     return process_transforms(image, query)
 
 # Old ITS Support
-# @app.route('/<namespace>/<path:image>')
-@app.route('/<namespace>/<path:image>.crop.<int:width>x<int:height>.<ext>')
+@app.route('/<namespace>/<path:filename>.crop.<int:width>x<int:height>.<ext>')
 def crop(namespace, image, width, height, ext):
-	pass
+    query = {'crop':width + 'x' + height,'format':str(ext)}
+    # query['filename'] = filename
 
-@app.route('/<namespace>/<path:image>.fit.<int:width>x<int:height>.<ext>')
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
+
+@app.route('/<namespace>/<path:filename>.fit.<int:width>x<int:height>.<ext>')
 def fit(namespace, image, width, height, ext):
-	pass
+	query = {'fit':width + 'x' + height,'format':str(ext)}
+    # query['filename'] = filename
+
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
 
 @app.route('/<namespace>/<path:image>.focalcrop.<int:width>x<int:height>.<int(min=0,max=100):x>.<int(min=0,max=100):y>.<ext>')
 def focalcrop(namespace, image, width, height, x, y, ext):
-	pass
+    query = {'crop':width + 'x' + height + 'x' + x + 'x' + y,'format':str(ext)}
+    # query['filename'] = filename
+
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
 
 @app.route('/<namespace>/<path:image>.resize.<int:width>x.passport.<ext>')
 def resize_width_passport(namespace, image, width, ext):
-	pass
+    query = {'resize':width + 'x','overlay':'passport','format':str(ext)}
+    # query['filename'] = filename
+
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
 
 @app.route('/<namespace>/<path:image>.resize.x<int:height>.passport.<ext>')
 def resize_height_passport(namespace, image, height, ext):
-	pass
+    query = {'resize':'x'+ height,'overlay':'passport','format':str(ext)}
+    # query['filename'] = filename
+
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
 
 @app.route('/<namespace>/<path:image>.resize.<int:width>x<int:height>.passport.<ext>')
 def resize_passport(namespace, image, width, height, ext):
-	pass
+    query = {'resize':width + 'x'+ height,'overlay':'passport','format':str(ext)}
+    # query['filename'] = filename
+
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
 
 @app.route('/<namespace>/<path:image>.resize.<int:width>x.<ext>')
 def resize_width(namespace, iamge, width, ext):
-	pass
+	query = {'resize':width + 'x','format':str(ext)}
+    # query['filename'] = filename
 
-@app.route('/<namespace>/<path:image>.resize.x<int:height>..<ext>')
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
+
+@app.route('/<namespace>/<path:image>.resize.x<int:height>.<ext>')
 def resize_height(namespace, image, height, ext):
-	pass
+	query = {'resize':'x' + height,'format':str(ext)}
+    # query['filename'] = filename
+
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
 
 @app.route('/<namespace>/<path:image>.resize.<int:width>x<int:height>.<ext>')
 def resize(namespace, image, width, height, ext):
-	pass
+	query = {'resize':width + 'x'+ height,'format':str(ext)}
+    # query['filename'] = filename
+
+    image = load_image(namespace, image)
+
+    if image is None:
+        abort(404)
+
+    # transformation processor call
+    return process_transforms(image, query)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
--- its/application_old_api.py
+++
@@ -1,3 +0,0 @@
-"""
-    Skeleton file for parsing old ITS style requests through Flask
-"""
