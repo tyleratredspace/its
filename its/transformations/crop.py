6e8a35dd6fd04b26f059e764538c3f9721415a50
Haley F. Sanders <hfsanders@pbs.org>
2017-06-27 11:14:13 -0500

    focal and smart crops use percentages instead of pixel coords

--- its/transformations/crop.py
+++ its/transformations/crop.py
@@ -13,13 +13,13 @@ class CropTransform(BaseTransform):
         The default focal point is the center of the image.
         """
         crop_args = args[0].split('x')
-        print(args)
-        if len(args) == 2:  # smart crop
+        
+        if len(args) == 2 and len(crop_args) == 2:  # smart crop
             filename = args[1][0]
             fname_split = filename.find("focus-")
             fname_args = filename[fname_split + len("focus-"):].split('_')
             fname_args[1] = fname_args[1].split(".")[0]
-            crop_args[2:3] = [int(fname_args[0]), int(fname_args[1])]
+            crop_args[2:3] = [fname_args[0], fname_args[1]]
 
         try:
             crop_size = [int(crop_args[0]), int(crop_args[1])]
@@ -28,6 +28,15 @@ class CropTransform(BaseTransform):
                 focal = [floor(img.size[0]/2), floor(img.size[1]/2)]
             else:  # focal crop
                 focal = [int(crop_args[2]), int(crop_args[3])]
+                # smart and focal should take in percentages
+                if focal[0] < 0 or focal[0] > 100:
+                    print("Focus arguments should be between 0 and 100")
+                    abort(400)
+                elif focal[1] < 0 or focal[1] > 100:
+                    print("Focus arguments should be between 0 and 100")
+                    abort(400)
+                focal[0] = floor((focal[0]/100) * img.size[0])
+                focal[1] = floor((focal[1]/100) * img.size[1])
         except Exception as e:
             print(e)
             abort(400)
@@ -37,6 +46,7 @@ class CropTransform(BaseTransform):
             if len(crop_args) == 2: # regular crop
                 img = img.crop([focal[0] - crop_size[0], focal[1] - crop_size[1], focal[0] + crop_size[0], focal[1] + crop_size[1]])
             else: # smart crop and focal crop
+
                 img = img.crop([focal[0], focal[1], focal[0] + crop_size[0], focal[1] + crop_size[1]])
         except Exception as e:
             print(e)
