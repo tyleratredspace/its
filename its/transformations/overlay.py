3e0d2f786d46c2e0bce4f19336bdd9f03ad11aa5
Haley F. Sanders <hfsanders@pbs.org>
2017-06-29 11:33:19 -0400

    Cleaning up according to code guidelines

--- its/transformations/overlay.py
+++ its/transformations/overlay.py
@@ -3,6 +3,7 @@ from PIL import Image
 from math import floor
 from pathlib import Path
 
+
 class OverlayTransform(BaseTransform):
 
     """
@@ -19,13 +20,21 @@ class OverlayTransform(BaseTransform):
         if overlay_args[2].lower() != "passport":
             if overlay_args[2].find('^') > 0:
                 overlay = overlay_args[2].split('^')
-                overlay_image = Image.open(Path(api_root + overlay[0]))
-                # check in cache for a version of overlay that has already been transformed
-                # do the temporary transforms on the overlay 
+                if overlay[0][0] == "/":
+                    overlay[0][0] = " "
+                    overlay[0].lstrip()
+
+                overlay_image = Image.open(Path(api_root / overlay[0]))
+                # check in cache for a version of overlay
+                # that has already been transformed
+                # do the temporary transforms on the overlay
             else:
                 overlay_image = Image.open(Path(api_root / overlay_args[2]))
         else:
-            overlay_image = Image.open(its_root / "static/Passport_Compass_Rose.png")
+            overlay_image = Image.open(
+                                        its_root /
+                                        "static/Passport_Compass_Rose.png"
+                                    )
 
         # placement of top left corner of overlay
         x_coord = floor((int(overlay_args[0]) / 100) * img.width)
@@ -34,12 +43,12 @@ class OverlayTransform(BaseTransform):
         # Only the overlay has an alpha channel
         if(img.mode != "RGBA"):
             new_img = img.copy()
-            new_img.paste(overlay_image,box=[x_coord,y_coord], mask=overlay_image)
         else:
-        # overlay and input img have alpha channels
-            new_img = Image.new("RGBA",img.size)
-            new_img = Image.alpha_composite(new_img,img)
-            new_img.paste(overlay_image,box=[x_coord,y_coord], mask=overlay_image)
+            # overlay and input img have alpha channels
+            new_img = Image.new("RGBA", img.size)
+            new_img = Image.alpha_composite(new_img, img)
+
+        new_img.paste(overlay_image, box=[x_coord, y_coord], mask=overlay_image)
 
         img = new_img
 
