b947e983bce7aedc7cdd77c8d37b9c2ddf139fdc
Haley F. Sanders <hfsanders@pbs.org>
2017-07-05 11:24:17 -0400

    simplified and shortened application.py

--- its/application.py
+++ its/application.py
@@ -2,10 +2,29 @@
 
 from flask import Flask, request, abort
 from pipeline import process_transforms
-from loader import load_image
+from loader import loader
 
 app = Flask(__name__)
 
+def process_request(namespace, query, filename):
+    query['filename'] = filename
+    image = loader(namespace, filename)
+
+    if image is None:
+        abort(404)
+
+    return process_transforms(image, query)
+
+def query_resize(width, height, ext):
+    query = {'resize':'x', 'format':str(ext)}
+
+    if width is not None:
+        query['resize'] = width + query['resize']
+
+    if height is not None:
+        query['resize'] = query['resize'] + height
+
+    return query
 
 # root
 @app.route('/')
@@ -17,133 +36,46 @@ def index():
 @app.route('/<namespace>/<path:filename>', methods=['GET'])
 def transform_image(namespace, filename):
     query = request.args.to_dict()
-    query['filename'] = filename
-
-    image = load_image(namespace, filename)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
+    result = process_request(namespace, query, filename)
+    return result
 
 # Old ITS Support
-@app.route('/<namespace>/<path:filename>.crop.<int:width>x<int:height>.<ext>')
-def crop(namespace, image, width, height, ext):
+@app.route('/<namespace>/<path:filename>.crop.<width>x<height>.<ext>')
+def crop(namespace, filename, width, height, ext):
     query = {'crop':width + 'x' + height,'format':str(ext)}
-    query['filename'] = filename
+    result = process_request(namespace, query, filename)
+    return result
 
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:filename>.fit.<int:width>x<int:height>.<ext>')
-def fit(namespace, image, width, height, ext):
-	query = {'fit':width + 'x' + height,'format':str(ext)}
-    query['filename'] = filename
-
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:image>.focalcrop.<int:width>x<int:height>.<int(min=0,max=100):x>.<int(min=0,max=100):y>.<ext>')
-def focalcrop(namespace, image, width, height, x, y, ext):
+@app.route('/<namespace>/<path:filename>.focalcrop.<width>x<height>.<int(min=0,max=100):x>.<int(min=0,max=100):y>.<ext>')
+def focalcrop(namespace, filename, width, height, x, y, ext):
     query = {'crop':width + 'x' + height + 'x' + x + 'x' + y,'format':str(ext)}
-    query['filename'] = filename
+    result = process_request(namespace, query, filename)
+    return result
 
-    image = load_image(namespace, image)
+@app.route('/<namespace>/<path:filename>.fit.<width>x<height>.<ext>')
+def fit(namespace, filename, width, height, ext):
+    query = {'fit':width + 'x' + height,'format':str(ext)}
+    result = process_request(namespace, query, filename)
+    return result
 
-    if image is None:
-        abort(404)
+# resize with pseduo-optional arguments
+@app.route('/<namespace>/<path:filename>.resize.<width>x<height>.<ext>')
+@app.route('/<namespace>/<path:filename>.resize.x<height>.<ext>')
+@app.route('/<namespace>/<path:filename>.resize.<width>x.<ext>')
+def resize(namespace, filename, ext, width=None, height=None):
+    query = query_resize(width, height, ext)
+    result = process_request(namespace, query, filename)
+    return result
 
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:image>.resize.<int:width>x.passport.<ext>')
-def resize_width_passport(namespace, image, width, ext):
-    query = {'resize':width + 'x','overlay':'passport','format':str(ext)}
-    query['filename'] = filename
-
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:image>.resize.x<int:height>.passport.<ext>')
-def resize_height_passport(namespace, image, height, ext):
-    query = {'resize':'x'+ height,'overlay':'passport','format':str(ext)}
-    query['filename'] = filename
-
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:image>.resize.<int:width>x<int:height>.passport.<ext>')
-def resize_passport(namespace, image, width, height, ext):
-    query = {'resize':width + 'x'+ height,'overlay':'passport','format':str(ext)}
-    query['filename'] = filename
-
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:image>.resize.<int:width>x.<ext>')
-def resize_width(namespace, iamge, width, ext):
-	query = {'resize':width + 'x','format':str(ext)}
-    query['filename'] = filename
-
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:image>.resize.x<int:height>.<ext>')
-def resize_height(namespace, image, height, ext):
-	query = {'resize':'x' + height,'format':str(ext)}
-    query['filename'] = filename
-
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
-
-@app.route('/<namespace>/<path:image>.resize.<int:width>x<int:height>.<ext>')
-def resize(namespace, image, width, height, ext):
-	query = {'resize':width + 'x'+ height,'format':str(ext)}
-    query['filename'] = filename
-
-    image = load_image(namespace, image)
-
-    if image is None:
-        abort(404)
-
-    # transformation processor call
-    return process_transforms(image, query)
+# passport overlay resize with pseduo-optional arguments
+@app.route('/<namespace>/<path:filename>.resize.<int:width>x<int:height>.passport.<ext>')
+@app.route('/<namespace>/<path:filename>.resize.x<int:height>.passport.<ext>')
+@app.route('/<namespace>/<path:filename>.resize.<int:width>x.passport.<ext>')
+def resize_passport(namespace, filename, width, height, ext):
+    query = query_resize(width, height, ext)
+    query['overlay'] ='passport'
+    result = process_request(namespace, query, filename)
+    return result
 
 if __name__ == '__main__':
-app.run(debug=True)
+    app.run(debug=True)
