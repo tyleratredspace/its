621a493e8e730d52de10466b99091faad332a083
Haley F. Sanders <hfsanders@pbs.org>
2017-07-07 14:08:31 -0400

    simple tests for all crop types

--- its/tests/test_pipeline.py
+++ its/tests/test_pipeline.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-from PIL import Image, ImageChops, ImageMath
+from PIL import Image, ImageChops
 from pathlib import Path
+import itertools
 from math import sqrt
 from pipeline.pipeline import process_transforms
 
@@ -18,42 +19,51 @@ class TestCropTransform(TestCase):
     # @classmethod
     # def tearDownClass(self):
 
-    # @classmethod
-    # def eu_norm(self, image):
+    @classmethod
+    def eu_norm(self, image):
         """
-        Calculates the euclidean norm of the pixels of an image
+        Calculates the euclidean norm of the pixels of a grayscale image
         """
-        # coords = list()
+        # List of all the possible coordinates in the image
+        coords = list(itertools.product(range(image.width), range(image.height)))
+        # list of all pixels in the image
+        pixels = [ image.getpixel(coord) for coord in coords ]
+        norm = sum([ pow(sqrt(pix), 2 ) for pix in pixels ])
 
-        # for x in range(1, image.width):
-        #     for y in range(1, image.height):
-        #         coords.append((x,y))
-        # pixels = [ image.getpixel(coord) for coord in coords ]
+        return norm
 
-        # norm = [ pow( pow(pix, 0.5), 2 ) for pix in pixels ]
-
-        # norm = sum(norm)
-
-        # return norm
-
-    def test_default_crop(self):
-        test_image = Image.open(self.img_dir / "top_left.png")
-        test_image.info['filename'] = "top_left.png"
+    def test_default_crop_no_alpha(self):
+        test_image = Image.open(self.img_dir / "middle.png")
+        test_image.info['filename'] = "middle.png"
         query = {'crop':'1x1'}
         expected = Image.open(self.img_dir / "expected/blue_pixel.png")
-        actual = process_transforms(test_image, query) 
+        actual = process_transforms(test_image, query)
         diff = ImageChops.difference(expected, actual)
-        # norm = self.eu_norm(diff) # calculate the norm
-        norm = 0
-        self.assertEqual(0, norm)
+        diff_gray = diff.convert('L') # convert to grayscale, 'LA' for L with alpha channel
+        norm = self.eu_norm(diff_gray) # calculate the norm
+        self.assertNotAlmostEqual(0, norm) # the difference would be 0 if they were the same
 
-    # def test_focal_crop(self):
+    def test_focal_crop(self):
+        test_image = Image.open(self.img_dir / "top_left.png")
+        test_image.info['filename'] = "top_left.png"
+        query = {'crop':'1x1x0x0'}
+        expected = Image.open(self.img_dir / "expected/blue_pixel.png")
+        actual = process_transforms(test_image, query)
+        diff = ImageChops.difference(expected, actual)
+        diff_gray = diff.convert('L') # convert to grayscale, 'LA' for L with alpha channel
+        norm = self.eu_norm(diff_gray) # calculate the norm
+        self.assertAlmostEqual(0, norm)
 
-    #   self.Assert
-
-    # def test_smart_crop(self):
-
-    #   self.Assert
+    def test_smart_crop(self):
+        test_image = Image.open(self.img_dir / "bottom_right_focus-99x99.png")
+        test_image.info['filename'] = "bottom_right_focus-99x99.png"
+        query = {'crop':'1x1'}
+        expected = Image.open(self.img_dir / "expected/blue_pixel.png")
+        actual = process_transforms(test_image, query)
+        diff = ImageChops.difference(expected, actual)
+        diff_gray = diff.convert('L') # convert to grayscale, 'LA' for L with alpha channel
+        norm = self.eu_norm(diff_gray) # calculate the norm
+        self.assertAlmostEqual(0, norm)
 
 # class TestOverlayTransform(TestCase):
 #   """
