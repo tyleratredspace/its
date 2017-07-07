e5daa46efe6d6e52c36233faef8539585053f333
Haley F. Sanders <hfsanders@pbs.org>
2017-07-07 16:51:47 -0400

    added integrity resize test

--- its/tests/test_pipeline.py
+++ its/tests/test_pipeline.py
@@ -5,15 +5,32 @@ import itertools
 from math import sqrt
 from pipeline.pipeline import process_transforms
 
+def get_pixels(image):
+    # List of all the possible coordinates in the image
+    coords = list(itertools.product(range(image.width), range(image.height)))
+    # list of all pixels in the image
+    pixels = [ image.getpixel(coord) for coord in coords ]
+
+    return pixels
+
+def compare_pixels(img1, img2):
+
+    img1_pixels = get_pixels(img1)
+    img2_pixels = get_pixels(img2)
+    matches = 0
+    total = len(img1_pixels) # both images should have the same number of pixels
+
+    for pixel_pair in zip(img1_pixels, img2_pixels):
+        if pixel_pair[0] == pixel_pair[1]:
+            matches += 1
+
+    return matches / total
 
 def eu_norm(image):
     """
     Calculates the euclidean norm of the pixels of a grayscale image
     """
-    # List of all the possible coordinates in the image
-    coords = list(itertools.product(range(image.width), range(image.height)))
-    # list of all pixels in the image
-    pixels = [ image.getpixel(coord) for coord in coords ]
+    pixels = get_pixels(image)
     norm = sum([ pow(sqrt(pix), 2 ) for pix in pixels ])
 
     return norm
@@ -76,24 +93,29 @@ class TestOverlayTransform(TestCase):
         norm = eu_norm(diff_gray) # calculate the norm
         self.assertAlmostEqual(0, norm)
 
-# class TestResizeTransform(TestCase):
-#   """
-#   """
-#     @classmethod
-#     def setUpClass(self):
-#         # current directory / images
-#         self.img_dir = Path(__file__).parents[0] / "images"
+class TestResizeTransform(TestCase):
 
-#     def test_resize(self):
-#         test_image = Image.open(self.img_dir / "bottom_right_focus-99x99.png")
-#         test_image.info['filename'] = "bottom_right_focus-99x99.png"
-#         query = {'crop':'1x1'}
-#         expected = Image.open(self.img_dir / "expected/blue_pixel.png")
-#         actual = process_transforms(test_image, query)
-#         diff = ImageChops.difference(expected, actual)
-#         diff_gray = diff.convert('L') # convert to grayscale, 'LA' for L with alpha channel
-#         norm = self.eu_norm(diff_gray) # calculate the norm
-#         self.assertAlmostEqual(0, norm)
+    @classmethod
+    def setUpClass(self):
+        # current directory / images
+        self.img_dir = Path(__file__).parents[0] / "images"
+        self.threshold = 0.5
+
+    def test_resize_size(self):
+        test_image = Image.open(self.img_dir / "abstract.png")
+        test_image.info['filename'] = "abstract.png"
+        query = {'resize':'10x10'}
+        result = process_transforms(test_image, query)
+        self.assertEqual(result.size, (10, 10))
+
+    def test_resize_integrity(self):
+        test_image = Image.open(self.img_dir / "test.png")
+        test_image.info['filename'] = "test.png"
+        query = {'resize':'100x100'}
+        expected = Image.open(self.img_dir / "expected/test_resize.png")
+        actual = process_transforms(test_image, query)
+        comparison = compare_pixels(expected, actual)
+        self.assertGreaterEqual(comparison, self.threshold)
 
 if __name__ == '__main__':
     unittest.main()
