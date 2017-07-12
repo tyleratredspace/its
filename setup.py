6942dd0f21b412127b1afcf68f4fd55f85ac7844
James Turk <james.p.turk@gmail.com>
2017-07-12 14:17:59 -0400

    add tox.ini and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="its",
+      version='2.0.0',
+      py_modules=['its'],
+      author="",
+      author_email='',
+      license="MIT",
+      url="",
+      long_description="",
+      packages=find_packages(),
+      description="image transformation service",
+      platforms=["any"],
+      install_requires=[
+          'flask',
+          'pillow',
+      ],
+      extras_require={
+          'dev': [
+              'flake8',
+              'pytest',
+          ]
+      },
+      )
