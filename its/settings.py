fa520f0f2962d4225dc2ff1c8a5579998f33c2e7
Haley F. Sanders <hfsanders@pbs.org>
2017-08-04 10:57:28 -0400

    reworking tests due to code changes

--- its/settings.py
+++ its/settings.py
@@ -1,23 +1,24 @@
+import os
 
 
 # Set DEBUG = True to enable debugging application.
-DEBUG = True
+DEBUG = os.environ.get('ITS_DEBUG', 'false').lower() == 'true'
 
 MIME_TYPES = {
-    'PNG': 'image/png',
-    'JPEG': 'image/jpeg',
-    'JPG': 'image/jpeg',
-    'WEBP': 'image/webp',
-    'SVG': 'image/svg+xml'
+    'PNG': os.environ.get('ITS_MIME_TYPE_PNG', 'image/png'),
+    'JPEG': os.environ.get('ITS_MIME_TYPE_JPEG', 'image/jpeg'),
+    'JPG': os.environ.get('ITS_MIME_TYPE_JPEG', 'image/jpeg'),
+    'WEBP': os.environ.get('ITS_MIME_TYPE_WEBP', 'image/webp'),
+    'SVG': os.environ.get('ITS_MIME_TYPE_WEBP', 'image/svg+xml')
 }
 
-PNGQUANT_PATH = "pngquant"
+PNGQUANT_PATH = os.environ.get('ITS_PNGQUANT_PATH', "pngquant")
 
-DEFAULT_JPEG_QUALITY = 95
+DEFAULT_JPEG_QUALITY = int(os.environ.get('ITS_DEFAULT_JPEG_QUALITY', "95"))
 
-IMAGE_LOADER = 'file_system'
+IMAGE_LOADER = os.environ.get('ITS_IMAGE_LOADER', 's3')
 
-OVERLAY_LOADER = 'file_system'
+OVERLAY_LOADER = os.environ.get('ITS_OVERLAY_LOADER', 's3')
 
 BUCKETS = {
 	'default': 'default',
@@ -28,12 +29,15 @@ BUCKETS = {
 }
 
 OVERLAYS = {
-    'passport': "static/Passport_Compass_Rose.png",
+    'overlay': "/static/overlay.png",
 }
 
-OVERLAY_PLACEMENT = [50, 50]
+OVERLAY_PLACEMENT = [
+    int(os.environ.get('ITS_OVERLAY_PLACEMENT_X', '5')),
+    int(os.environ.get('ITS_OVERLAY_PLACEMENT_Y', '5')) 
+    ]
 
 # the keyword used to recognize focal point args in filenames
-FOCUS_KEYWORD = "focus-"
+FOCUS_KEYWORD = os.environ.get('ITS_FOCUS_KEYWORD', "focus-")
 
-DELIMITERS_RE = "[x_,]"
+DELIMITERS_RE = os.environ.get('ITS_DELIMITERS_RE', '[x_,]')
