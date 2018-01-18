3400708f9bf60b4f4fea63d543f1e9c8c34122b9
James Turk <james.p.turk@gmail.com>
2018-01-18 12:42:23 -0500

    fix s3 loader bucket setting

--- its/loaders/s3_loader.py
+++ its/loaders/s3_loader.py
@@ -10,7 +10,7 @@ from ..settings import NAMESPACES
 class S3Loader(BaseLoader):
 
     slug = "s3"
-    parameter_name = "buckets"
+    parameter_name = "bucket"
 
     @staticmethod
     def get_fileobj(namespace, filename):
@@ -21,13 +21,7 @@ class S3Loader(BaseLoader):
         # get the s3 resource
         s3 = boto3.resource('s3')
 
-        bucket_name = filename.split('/')[0]
-
-        if bucket_name in NAMESPACES[namespace][S3Loader.parameter_name]:
-            # get the specified bucket
-            bucket = s3.Bucket(NAMESPACES[namespace][S3Loader.parameter_name][bucket_name])
-        else:
-            raise NotFoundError("Namespace {} is not configured.".format(namespace))
+        bucket = s3.Bucket(NAMESPACES[namespace][S3Loader.parameter_name])
 
         # create an empty bytes object to store the image bytes in
         file_obj = BytesIO()
