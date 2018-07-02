d8499c9f515120b8372ef90c39895b58e799db69
cmac1000 <cmac1000@gmail.com>
2018-07-02 10:56:34 -0400

    type annotations and refactoring for optimize.py (#122)
    * type annotations and refactoring for optimize.py
    
    * specify output for optimize.optimize
    
    * remove png logic from optimize.optimize
    
    * format

--- its/optimize.py
+++ its/optimize.py
@@ -1,11 +1,14 @@
 import subprocess
-import tempfile
 import uuid
 from io import BytesIO
 from math import floor
 from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Any, Dict, Optional, Union
 
 from PIL import Image, ImageFile
+from PIL.JpegImagePlugin import JpegImageFile
+from PIL.PngImagePlugin import PngImageFile
 
 from its.settings import DEFAULT_JPEG_QUALITY, PNGQUANT_PATH
 
@@ -14,105 +17,109 @@ from .errors import ITSClientError, ITSTransformError
 ImageFile.MAXBLOCK = 2 ** 20  # for JPG progressive saving
 
 
-def optimize(img, query):
+def optimize(
+    img: Union[Image.Image, BytesIO], query: Dict[str, str]
+) -> Union[Image.Image, BytesIO]:
 
-    if not isinstance(img, BytesIO):
-        ext = (
-            query["format"] if "format" in query else img.format.lower()
-        )  # the return format
-        try:
-            quality = int(query["quality"]) if "quality" in query else None
-        except ValueError as error:
-            raise ITSClientError("ITS Client Error: " + str(error))
+    if isinstance(img, BytesIO):
+        return img
 
-        with tempfile.NamedTemporaryFile(dir="/tmp/", delete=True) as tmp_file:
-            if ext.lower() == "jpg":
-                ext = "jpeg"
+    ext = (
+        query["format"] if "format" in query else img.format.lower()
+    )  # the return format
+    try:
+        quality = int(query["quality"]) if "quality" in query else None
+    except ValueError as error:
+        raise ITSClientError("ITS Client Error: " + str(error))
 
-            # convert first, then optimize
-            if ext.lower() == "jpeg":
-                # convert to JPG and/or compress
-                # need to convert to RGB first, then can save in any format
-                if img.mode in ["RGBA", "LA"]:
-                    new_img = Image.new("RGBA", img.size)
-                    new_img = Image.alpha_composite(new_img, img)
-                img = img.convert("RGB")
-                img = optimize_jpg(img, tmp_file, quality)
-            elif ext.lower() in ["svg", "png", "webp"]:
-                # convert from PNG, JPG and WEBP to formats other than JPG
-                img = convert(img, ext, tmp_file)
+    with NamedTemporaryFile(dir="/tmp/", delete=True) as tmp_file:
+        if ext.lower() == "jpg":
+            ext = "jpeg"
 
-            else:
-                raise ITSClientError(
-                    "ITS Client Error: Format must be jpeg, svg, png or webp"
-                )
+        # convert first, then optimize
+        if ext.lower() == "jpeg":
+            # convert to JPG and/or compress
+            # need to convert to RGB first, then can save in any format
+            if img.mode in ["RGBA", "LA"]:
+                new_img = Image.new("RGBA", img.size)
+                new_img = Image.alpha_composite(new_img, img)
+            img = img.convert("RGB")
+            img = optimize_jpg(img, tmp_file, quality)
+        elif ext.lower() in ["png", "webp"]:
+            # convert from PNG, JPG and WEBP to formats other than JPG
+            img = convert(img, ext, tmp_file)
 
-            # only optimize pngs if quality param is provided
-            if img.format == "PNG" and quality is not None:
-                img = optimize_png(img, tmp_file, quality)
+        else:
+            raise ITSClientError("ITS Client Error: Format must be jpeg, png or webp")
+
+        # only optimize pngs if quality param is provided
+        if img.format == "PNG" and quality is not None:
+            img = optimize_png(img, tmp_file, quality)
 
     return img
 
 
-def convert(img, ext, tmp_file):
-    if ext.lower() != img.format.lower():  # same format so do nothing
-        if img.format.lower() in ["png", "webp", "jpeg"]:
-            if img.mode in ["CMYK", "LA"]:
-                img = img.convert("RGB")
-            img.save(tmp_file.name, ext.upper())
-            # reopen newly converted or compressed image
-            img = Image.open(tmp_file.name)
-    return img
+# https://github.com/python/mypy/issues/3094 - _TemporaryFileWrapper from
+# tempfile can't be imported for type annotation here
+def convert(img: Image.Image, ext: str, tmp_file: Any) -> Image.Image:
+    if ext.lower() == img.format.lower():  # same format so do nothing
+        return img
 
+    if img.format.lower() not in ["png", "webp", "jpeg"]:
+        return img
 
-def optimize_jpg(img, tmp_file, quality=None):
-    if quality is not None and quality <= 95:
-        img.save(
-            tmp_file.name, "JPEG", quality=quality, optimize=True, progressive=True
-        )
-    else:
-        # 95 is the reccommended upper limit on quality for JPEGs in PIL
-        img.save(
-            tmp_file.name,
-            "JPEG",
-            quality=DEFAULT_JPEG_QUALITY,
-            optimize=True,
-            progressive=True,
-        )
+    if img.mode in ["CMYK", "LA"]:
+        img = img.convert("RGB")
+
+    img.save(tmp_file.name, ext.upper())
+
     # reopen newly converted or compressed image
-    img = Image.open(tmp_file.name)
-
-    return img
+    return Image.open(tmp_file.name)
 
 
-def optimize_png(img, tmp_file, quality=None):
+def optimize_jpg(
+    img: JpegImageFile, tmp_file: Any, quality: Optional[int] = DEFAULT_JPEG_QUALITY
+) -> JpegImageFile:
+    if not quality:
+        quality = DEFAULT_JPEG_QUALITY
+    elif quality > 95:
+        quality = 95  # 95 is the recommended upper limit on quality for JPEGs in PIL
+
+    img.save(tmp_file.name, "JPEG", quality=quality, optimize=True, progressive=True)
+
+    return Image.open(tmp_file.name)
+
+
+def optimize_png(img: PngImageFile, tmp_file: Any, quality: int = 95) -> PngImageFile:
     output_path = "/tmp/" + str(uuid.uuid4())
 
-    if quality < 100:
-        # return the image as is since 100% quality means no compression
-        if quality % 10 == 0:
-            speed = int(quality / 10)  # quality is inversely related to pngquant speed
-        else:
-            # each interval of 10 is related to one speed:
-            # [90 - 99] --> speed 9
-            speed = int(floor((quality - (quality % 10)) / 10))
+    if quality >= 100:
+        return img
 
-        command = [
-            PNGQUANT_PATH,
-            "--strip",
-            "--force",
-            "--output",
-            output_path,
-            "-s" + str(speed),
-            tmp_file.name,
-        ]
+    if quality % 10 == 0:
+        speed = int(quality / 10)  # quality is inversely related to pngquant speed
+    else:
+        # each interval of 10 is related to one speed:
+        # [90 - 99] --> speed 9
+        speed = int(floor((quality - (quality % 10)) / 10))
 
-        try:
-            img.save(tmp_file.name, format=img.format)
-            subprocess.check_output(command, stderr=subprocess.STDOUT)
-            img = Image.open(output_path)
-            if Path(output_path).exists():
-                Path(output_path).unlink()
-        except (OSError, subprocess.CalledProcessError) as error:
-            raise ITSTransformError("ITSTransform Error: " + str(error))
+    command = [
+        PNGQUANT_PATH,
+        "--strip",
+        "--force",
+        "--output",
+        output_path,
+        "-s" + str(speed),
+        tmp_file.name,
+    ]
+
+    try:
+        img.save(tmp_file.name, format=img.format)
+        subprocess.check_output(command, stderr=subprocess.STDOUT)
+        img = Image.open(output_path)
+        if Path(output_path).exists():
+            Path(output_path).unlink()
+    except (OSError, subprocess.CalledProcessError) as error:
+        raise ITSTransformError("ITSTransform Error: " + str(error))
+
     return img
