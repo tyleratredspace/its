ab08e4d760ccc08466e571d628f6732482c30fe4
Bill Amberg <wamberg@pbs.org>
2018-09-06 15:10:32 -0400

    Test empty overlay.

--- its/tests/test_overlay.py
+++ its/tests/test_overlay.py
@@ -22,6 +22,12 @@ class TestOverlay(TestCase):
         )
         assert response.status_code == 200
 
+    def test_empty_overlay(self):
+        response = self.client.get(
+            "tests/images/test.png?overlay="
+        )
+        assert response.status_code == 200
+
     def test_legacy_passport_resize_overlay(self):
         response = self.client.get(
             "tests/images/test.png.resize.1000x1000.passport.png"
